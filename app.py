9f233712908df144732bebdb0521af3cd230c36b
Hadhazi-David <hadhazidavid.work@gmail.com>
2024-10-02 14:30:45 +0200

    commit5

---
+++ app.py
@@ -0,0 +1,29 @@
+import random
+eszkozok = ["ko", "papir", "ollo"]
+gepi_valasz = eszkozok[random.randint(0, 2)]
+print("Ko, papir, ollo")
+# print(gepi_valasz)
+bevitel = input("Valassz! ko/papir/ollo: ")
+while not bevitel in eszkozok:
+    print(">>> irjad normalisan <<<")
+    bevitel = input("Valassz! ko/papir/ollo: ")
+if bevitel in eszkozok:
+    if bevitel == gepi_valasz:
+        print("dontetlen!haha")
+    elif gepi_valasz == "ko":
+        if bevitel == "papir":
+            print("Grat, nyertel, ez volt a gep valasza: ", gepi_valasz)
+        else:
+            print("SZAR VAGY, ez volt a gep valasza: ", gepi_valasz)
+    elif gepi_valasz == "papir":
+        if bevitel == "ollo":
+            print("Grat, nyertel")
+        else:
+            print("SZAR VAGY, ez volt a gep valasza: ", gepi_valasz)
+    elif gepi_valasz == "ollo":
+        if bevitel == "ko":
+            print("Grat, nyertel")
+        else:
+            print("SZAR VAGY, ez volt a gep valasza: ", gepi_valasz)
+else:
+    print(">>> irjad normalisan <<<")
